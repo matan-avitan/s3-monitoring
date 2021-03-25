881b3d0159eb66ce8899da7193642db8a2155f1a
MatanAvitan-98 <matan3751@gmail.com>
2021-03-26 01:27:12 +0200

    add get module name

--- services/s3_downloader_service.py
+++ services/s3_downloader_service.py
@@ -7,13 +7,16 @@ class S3DownloaderService(S3ConnectionService):
 
     def __init__(self):
         super(S3DownloaderService, self).__init__()
+        self.name = "S3-Downloader"
 
     def download_file(self, file):
+        self.logger.info(f"download file: {file}", self.get_module())
         try:
-            print(f"download {file}")
             with open(file, 'wb+') as f:
                 self.connect.download_fileobj(self.bucket_name, file, f)
         except ClientError as e:
+            self.logger.error(f"{e}", self.get_module())
+
             return False
         return True
 
