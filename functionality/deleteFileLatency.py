059b0bf96c48a9c396c56462a7ea4f84b95eded1
MatanAvitan-98 <matan3751@gmail.com>
2021-03-26 01:27:59 +0200

    update logs in functionality and add multiproccessing to run in parallel

--- functionality/deleteFileLatency.py
+++ functionality/deleteFileLatency.py
@@ -7,8 +7,10 @@ class DeleteFileLatency(S3DeleteService):
 
     def __init__(self):
         super(DeleteFileLatency, self).__init__()
+        self.name = 'S3-Delete-Latency'
 
     @latency_check
     def run_functionality(self):
+        self.logger.info("start deleting", {'module': self.name})
         files = ['test_files/test.txt']
         self.delete_files(files)
--- functionality/downloadFileLatency.py
+++ functionality/downloadFileLatency.py
@@ -7,8 +7,12 @@ class DownloadFileLatency(S3DownloaderService):
 
     def __init__(self):
         super(DownloadFileLatency, self).__init__()
+        self.name = 'S3-Download-Latency'
+
 
     @latency_check
     def run_functionality(self):
+        self.logger.info("start download", {'module': self.name})
+
         files = ['test_files/test.txt']
         self.download_files(files)
