ceea88e5829a069d3ae36ca92eb49ed5c11678c4
MatanAvitan-98 <matan3751@gmail.com>
2021-03-26 12:43:20 +0300

    send logs insted of printing

--- functionality/utils.py
+++ functionality/utils.py
@@ -1,3 +1,4 @@
+import requests
 from datetime import datetime
 
 
@@ -7,6 +8,12 @@ def latency_check(function):
         function(*args, **kwargs)
         end_time = datetime.now()
         process_time = end_time - start_time
-        print(f'the process (download) take - {process_time}')
+        log = {
+            'insertion_time': datetime.now(),
+            'status': 'success',
+            'msg': f'the process (download) take - {process_time}'
+        }
+
+        requests.post('http://127.0.0.1:8080/api/log/', log)
 
     return wrapper
